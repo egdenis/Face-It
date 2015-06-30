b0062e82aa959afac0b0ca19398a4581c4e7376d
Etienne Denis <Etienne@pc16.home>
2015-06-30 23:35:41 +0200

    Game was working but then bugged

--- Face It/GameViewController.swift
+++ Face It/GameViewController.swift
@@ -10,101 +10,125 @@ import UIKit
 import QuartzCore
 import SceneKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController,UIGestureRecognizerDelegate,SCNPhysicsContactDelegate, SCNSceneRendererDelegate {
+    var scnView: SCNView!
+    var scn: Primitive!
+    var spheres:Array<SCNNode>!
+    var count: Double!
+    var maxCount: Double!
+    var box: SCNNode!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.dae")!
-        
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
-        
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = SCNLightTypeOmni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = SCNLightTypeAmbient
-        ambientLightNode.light!.color = UIColor.darkGrayColor()
-        scene.rootNode.addChildNode(ambientLightNode)
-        
-        // retrieve the ship node
-        let ship = scene.rootNode.childNodeWithName("ship", recursively: true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(0, y: 2, z: 0, duration: 1)))
-        
+      
         // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
+        self.scn = Primitive()
         // set the scene to the view
-        scnView.scene = scene
-        
-        // allows the user to manipulate the camera
-        scnView.allowsCameraControl = true
-        
-        // show statistics such as fps and timing information
-        scnView.showsStatistics = true
-        
-        // configure the view
-        scnView.backgroundColor = UIColor.blackColor()
-        
+        self.scnView = self.view as! SCNView
+        self.scnView.scene = self.scn
+        self.scnView.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 250/255, alpha: 1.0)
+        self.scnView.antialiasingMode = SCNAntialiasingMode.Multisampling4X
+        self.scnView.delegate = self
+        self.spheres = []
+        self.count = 0.0
+        self.maxCount = 20.0
+        self.box = self.scn.rootNode.childNodeWithName("box", recursively: true)
+
+        // configure the vie
         // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
-        var gestureRecognizers = [AnyObject]()
-        gestureRecognizers.append(tapGesture)
-        if let existingGestureRecognizers = scnView.gestureRecognizers {
-            gestureRecognizers.extend(existingGestureRecognizers)
-        }
-        scnView.gestureRecognizers = gestureRecognizers
+        var leftSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+        var rightSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+        var upSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+        var downSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+    
+        leftSwipe.direction = .Left
+        rightSwipe.direction = .Right
+        upSwipe.direction = .Up
+        downSwipe.direction = .Down
+        
+        view.addGestureRecognizer(leftSwipe)
+        view.addGestureRecognizer(rightSwipe)
+        view.addGestureRecognizer(upSwipe)
+        view.addGestureRecognizer(downSwipe)
+       
     }
     
-    func handleTap(gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
+    
+    func renderer(aRenderer: SCNSceneRenderer, updateAtTime time: NSTimeInterval) {
+        print("octave est un gros pede")
+        self.maxCount = self.maxCount - 0.01
+        self.count = self.count + 1
+
+        let colors = [UIColor(red: 250/255, green: 1, blue: 153/255, alpha: 1.0),UIColor(red: 174/255, green: 34/255, blue: 34/255, alpha: 1.0),UIColor(red: 92/255, green: 157/255, blue: 160/255, alpha: 1.0)]
+        var position = [Float(0),Float(0),Float(0)]
+        position[Int(arc4random_uniform(3))] = Float(10)
+        if(self.count>=self.maxCount){
+            print("ocatave le pede")
+            self.addSphere(colors[Int(arc4random_uniform(3))], x: position[0], y: position[1], z: position[2])
+            self.count = 0
+        }
         
-        // check what nodes are tapped
-        let p = gestureRecognize.locationInView(scnView)
-        if let hitResults = scnView.hitTest(p, options: nil) {
-            // check that we clicked on at least one object
-            if hitResults.count > 0 {
-                // retrieved the first clicked object
-                let result: AnyObject! = hitResults[0]
-                
-                // get its material
-                let material = result.node!.geometry!.firstMaterial!
-                
-                // highlight it
-                SCNTransaction.begin()
-                SCNTransaction.setAnimationDuration(0.5)
-                
-                // on completion - unhighlight
-                SCNTransaction.setCompletionBlock {
-                    SCNTransaction.begin()
-                    SCNTransaction.setAnimationDuration(0.5)
-                    
-                    material.emission.contents = UIColor.blackColor()
-                    
-                    SCNTransaction.commit()
-                }
-                
-                material.emission.contents = UIColor.redColor()
-                
-                SCNTransaction.commit()
+        for (var i = self.spheres.endIndex-1 ; i>0; i-- ){
+            var position = self.spheres[i].position
+            if(position.x + position.y + position.z == 10){
+                self.spheres[i].removeFromParentNode()
+                self.spheres.removeAtIndex(i)
             }
         }
+
+    }
+    
+    func addSphere(color: UIColor, x:Float, y:Float, z:Float){
+        let materialColor  = SCNMaterial()
+        materialColor.diffuse.contents = color
+        materialColor.locksAmbientWithDiffuse = true;
+        
+        let sphereGeometry = SCNSphere(radius:0.25)
+        sphereGeometry.materials = [materialColor]
+        
+        let sphere = SCNNode(geometry: sphereGeometry)
+        let sphereShape = SCNPhysicsShape(geometry: sphereGeometry, options: nil)
+        let sphereBody = SCNPhysicsBody(type: .Kinematic, shape: sphereShape)
+        
+        sphere.position = SCNVector3Make(x,y,z)
+        sphere.physicsBody = sphereBody;
+        self.scn.rootNode.addChildNode(sphere)
+        self.spheres.append(sphere)
+        sphere.runAction(SCNAction.moveTo(SCNVector3(x:0,y:0,z:0), duration: NSTimeInterval(2)))
+    }
+
+    
+    func handleTap(gestureRecognize: UIGestureRecognizer) {
+      
+    }
+    
+    func handleSwipes(sender:UISwipeGestureRecognizer) {
+        var location = sender.locationOfTouch(0, inView: self.scnView)
+        var spin = CABasicAnimation(keyPath: "rotation")
+
+        var x = Float(0.0), y = Float(0.0), z = Float(0.0)
+        var halfWidth = Float(self.scnView.bounds.width/2)
+        switch (sender.direction,Float(location.x)) {
+        case (UISwipeGestureRecognizerDirection.Left,_):
+            y = 1
+        case (UISwipeGestureRecognizerDirection.Right,_):
+            y = -1
+        case let (UISwipeGestureRecognizerDirection.Up, xPos) where xPos <= halfWidth:
+            x = 1
+        case let (UISwipeGestureRecognizerDirection.Up, xPos) where xPos > halfWidth:
+            z = -1
+        case let (UISwipeGestureRecognizerDirection.Down, xPos) where xPos <= halfWidth:
+            x = -1
+        case let (UISwipeGestureRecognizerDirection.Down, xPos) where xPos > halfWidth:
+            z = 1
+        default:
+            break
+        }
+
+        let action = SCNAction.rotateByAngle(CGFloat(0.5*M_PI), aroundAxis: SCNVector3(x: x, y: y, z: z), duration: NSTimeInterval(0.15))
+        self.box?.runAction(action)
+        
     }
     
     override func shouldAutorotate() -> Bool {
---
+++ Face It/Primitive.swift
@@ -0,0 +1,60 @@
+//
+//  Primitive.swift
+//  Face It
+//
+//  Created by Etienne Denis on 6/2/15.
+//  Copyright (c) 2015 Etienne Denis. All rights reserved.
+//
+
+import SceneKit
+import UIKit
+
+class Primitive: SCNScene {
+    var box:SCNNode!
+    override init() {
+        super.init()
+        let boxGeometry = SCNBox(width: 2.5, height: 2.5, length: 2.5, chamferRadius: 0.0)
+       
+        let greenMaterial = SCNMaterial()
+        greenMaterial.diffuse.contents = UIColor(red: 96/255, green: 157/255, blue: 160/255, alpha: 1.0)
+        greenMaterial.locksAmbientWithDiffuse = true;
+        
+        let redMaterial = SCNMaterial()
+        redMaterial.diffuse.contents = UIColor(red: 178/255, green: 34/255, blue: 34/255, alpha: 1.0)
+        redMaterial.locksAmbientWithDiffuse = true;
+        
+        let blueMaterial  = SCNMaterial()
+        blueMaterial.diffuse.contents = UIColor(red: 254/255, green: 1, blue: 153/255, alpha: 1.0)
+        blueMaterial.locksAmbientWithDiffuse = true;
+        
+        boxGeometry.materials = [blueMaterial,redMaterial,blueMaterial,redMaterial,greenMaterial,greenMaterial]
+        
+        self.box = SCNNode(geometry: boxGeometry)
+        self.box.name = "box"
+        let boxShape = SCNPhysicsShape(geometry: boxGeometry, options: nil)
+        let boxBody = SCNPhysicsBody(type: .Kinematic, shape: boxShape)
+        
+        self.box.physicsBody = boxBody;
+        self.rootNode.addChildNode(self.box)
+        
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light!.type = SCNLightTypeAmbient
+        ambientLightNode.light!.color = UIColor(white: 0.47, alpha: 1.0)
+        self.rootNode.addChildNode(ambientLightNode)
+        
+       
+        
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3Make(10,10, 10)
+        cameraNode.eulerAngles = SCNVector3Make(Float(-0.6), Float(M_PI_4), Float(0))
+        self.rootNode.addChildNode(cameraNode)
+        
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
